6be2d1c38ffb045000fcce549a74d236ead8476f
NirvanaSatori <morris24kariuki@gmail.com>
2024-04-24 02:38:24 +0300

    use resources to map routes to the trans_actions controllers

--- app-backend/config/routes.rb
+++ app-backend/config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  match "/transactions", to: "trans_actions#method_not_allowed", via: [:put]
-  resources :trans_actions,  only: [:index, :show, :create],constraints: { method: ['GET', 'POST'] }
+  match "/trans_actions", to: "trans_actions#method_not_allowed", via: [:put]
+  resources :trans_actions,  only: [:index, :show, :create, :update, :destroy]
   resources :accounts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/transactions", to:"trans_actions#index"
-  get "/transactions/:id", to:"trans_actions#show"
-  post "/transactions", to:"trans_actions#create"
-  put "/transactions/:id", to:"trans_actions#update", via: [:patch]
-  delete "/transactions", to:"trans_actions#destroy"
   # Defines the root path route ("/")
   # root "articles#index"
   get '/ping', to: 'ping#show', format: :json, as: :ping
